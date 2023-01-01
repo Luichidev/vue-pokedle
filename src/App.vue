c82b45d911397993f48cdc7b029b809a63963f5c
Luis Alberto Arana <luichidev@gmail.com>
2023-01-01 23:20:02 +0100

    feat: Add OneLetter component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <script setup>
-import HomeView from './views/HomeView.vue'
+  import HomeView from './views/HomeView.vue'
+  import OneLetter from './components/OneLetter.vue';
 </script>
 
 <template>
   <main>
     <HomeView />
+    <OneLetter color='green' letter='A'/>
+    <OneLetter color='gold' letter='B'/>
+    <OneLetter color='grey' letter='C'/>
+    <OneLetter />
+
   </main>
 </template>
---
+++ src/components/OneLetter.vue
@@ -0,0 +1,49 @@
+<script setup>
+  defineProps({
+    letter: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    color: {
+      type: String,
+      default: 'transparent',
+    }
+  })
+</script>
+
+<template>
+  <div class="letter" :class='[`bg--${color}`]'>
+    {{ letter }}
+  </div>
+</template>
+
+<style scoped>
+.letter {
+    width: 8rem;
+    height: 8rem;
+    border: 1px solid black;
+    font-weight: bold;
+    font-size: 6rem;
+    padding: 2rem;
+  } 
+
+  .bg--transparent {
+    background-color: transparent;
+  }
+  .bg--green {
+    background-color: green;
+    border: 1px solid green;
+    color: #fff;
+  }
+  .bg--gold {
+    background-color: goldenrod;
+    border: 1px solid goldenrod;
+    color: #fff;
+  }
+  .bg--grey {
+    background-color: grey;
+    border: 1px solid grey;
+    color: #fff;
+  }
+</style>
